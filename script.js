c2ae67229a3c316eb567710ba064871ccbe95733
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-08 15:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function fibonacci(num) {
 // your code here
 	let a = 0;
   let b = 1;
-  let fib = 1;
-  for (let i = 2; i <= n; i++) {
-    fib = a + b;
+
+  for (let i = 2; i <= num; i++) {
+    let fib = a + b;
     a = b;
     b = fib;
   }
-  return fib;
+  return b;
 }
 
 module.exports = fibonacci;
